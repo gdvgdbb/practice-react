c1811a80bdc1150ec2207e9e777a6efc722b1097
chenxinyi <2166422858@qq.com>
2025-11-18 16:57:03 +0800

    第一天: 练习一些基础语法

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+
+const a = 1111
+const fn = () => 'Tom'
+const list = [
+  {
+    id: 1,
+    desc: '得即高歌失即休'
+  },
+  {
+    id: 2,
+    desc: '世间无限丹青手'
+  },
+  {
+    id: 3,
+    desc: '不及汪伦送我情'
+  },
+  {
+    id: 4,
+    desc: '一览众山小'
+  }
+]
+const flag = true
+const taggle = 2
+
+const fnc = () => {
+  if (taggle === 0) {
+    return <div>无图</div>
+  } else if (taggle === 1) {
+    return <div>一图</div>
+  } else {
+    return <div>多图</div>
+  }
+}
+
+const handleClick = (a, e) => {
+  console.log('button 被点击了', a, e);
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      this is App
+      { 'hello world' }
+      { 123 }
+      { a }
+      { fn() }
+      { new Date().getDate() }
+      <div style={{ color:'red' }}>My name id toni</div>
+      <ul>
+        { list.map(item => <li key={ item.id } style={{ color: 'skyblue' }}>{ item.desc }</li>) }
+      </ul>
+      { flag && <div>条件为真</div> }
+      { flag ? <div>真</div> : <div>假</div> }
+      { fnc() }
+      <button onClick={ (e) => handleClick('hello', e) }>点击我</button>
     </div>
   );
 }
