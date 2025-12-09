f3a5181b5695d91c9f88e4d0d136559622b8fb37
chenxinyi <2166422858@qq.com>
2025-12-09 14:51:47 +0800

    feat: 初识useEffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import { useState, useEffect } from "react";
 
 function App() {
+  const [list, setList] = useState([]);
+  useEffect( () => {
+    async function getList() {
+      const data = await fetch("http://geek.itheima.net/v1_0/channels");
+      const json = await data.json();
+      console.log(data, json);
+      setList(json.data.channels);
+    }
+    getList();
+  }, [])
   return (
     <div className="App">
-      
+      <ul>
+        {list.map(item => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
