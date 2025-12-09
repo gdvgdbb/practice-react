88b9ee227ee9d92c0380652afe9d8cffcfffb7f3
chenxinyi <2166422858@qq.com>
2025-12-09 14:05:07 +0800

    feat: 使用createContext和useContext进行深层级的数据传递

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
+import { createContext, useContext } from "react";
+const MsgContext = createContext("");
+
+function A() {
+  const mesg = useContext(MsgContext);
+  return (
+    <>
+      <div>我是A</div>
+      {mesg}
+      <B />
+    </>
+  )
+}
+
+function B() {
+  const msg = useContext(MsgContext);
+  console.log(msg);
+  return (
+    <div>
+      我是B
+      <span>{msg}</span>
+    </div>
+  )
+}
 
 function App() {
+  const name = "小明";
+  const age = 17
   return (
     <div className="App">
-      
+      <MsgContext.Provider value={[name, age]}>
+        <A />
+      </MsgContext.Provider>
     </div>
   );
 }
