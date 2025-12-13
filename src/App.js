18aac122fe19d157d88ea58d104a64fafcb74c21
蓝曦 <2166422858@qq.com>
2025-12-13 21:30:59 +0800

    feat: 双向绑定

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-
+import { useState, useRef } from "react";
 function App() {
+  const [value, setValue] = useState();
+  const inputRef = useRef();
+  const handleClick = () => {
+    console.log(inputRef.current);
+  }
   return (
     <div className="App">
-      
+      <input
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        ref={inputRef}
+        onClick={handleClick}
+      />
     </div>
   );
 }
