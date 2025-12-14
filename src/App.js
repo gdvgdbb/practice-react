bccf3091c80280fea812a1c568a08a927dd787fb
蓝曦 <2166422858@qq.com>
2025-12-14 15:51:55 +0800

    feat: 学习hook函数封装以及使用方法

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-
+import { useState } from "react";
+function useTaggle () {
+  const [value, setvalue] = useState(true);
+  const toggleValue = () => {
+    setvalue(!value);
+  }
+  // return { value, toggleValue };
+  return [value, toggleValue];
+}
 function App() {
+  const { value, toggleValue } = useTaggle();
   return (
     <div className="App">
-      
+      { value && <h1>Hello World!</h1> }
+      <button onClick={toggleValue}>点击切换</button>
     </div>
   );
 }
