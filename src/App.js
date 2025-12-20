8783341af6e83c8a3f07cf9a9b955f024c600481
蓝曦 <2166422858@qq.com>
2025-12-20 15:47:20 +0800

    feat: 加入第三方库进行数据处理

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-import { useState } from 'react';
-import _ from 'lodash';
+import { useState, useEffect } from 'react';
+import _, { get } from 'lodash';
 import classNames from 'classnames';
 import './index.css';
+import axios from 'axios';
 
 function App() {
-  const listDate = [
-    { id: 1, name: 'Item 1', goodNum: 100, hotNum: 300 }, 
-    { id: 2, name: 'Item 2', goodNum: 200, hotNum: 100 },
-    { id: 3, name: 'Item 3', goodNum: 50, hotNum: 500 }
-  ]
-  const [list, setList] = useState(_.orderBy(listDate, 'goodNum', 'desc'));
+  useEffect(() => {
+    async function getList() {
+      const res = await axios.get('http://localhost:4000/listDate')
+      setList(res.data)
+    }
+    getList()
+  },[])
+  // const listDate = [
+  //   { id: 1, name: 'Item 1', goodNum: 100, hotNum: 300 }, 
+  //   { id: 2, name: 'Item 2', goodNum: 200, hotNum: 100 },
+  //   { id: 3, name: 'Item 3', goodNum: 50, hotNum: 500 }
+  // ]
+  // const [list, setList] = useState(_.orderBy(listDate, 'goodNum', 'desc'));
+  const [list, setList] = useState([]);
   const id = 2;
   const [typeId, setTypeId] = useState(1);
-  const [typeList] = useState([
+  const typeList = [
     { id: 1, name: '最新' },
     { id: 2, name: '最热' }
-  ])
+  ]
 
   const handleDelete = (itemId) => {
     setList(list.filter(item => item.id !== itemId))
